db93f2252e6e40ad351a86cfd73fc2d0ee54c5e5
itsneiq <fauzanozan10@gmail.com>
2026-07-28 08:47:31 +0700

    feat(export): expose native media file paths

--- apps/auto-editor-v1/electron/preload.js
+++ apps/auto-editor-v1/electron/preload.js
@@ -1,9 +1,10 @@
 "use strict";
 
-const { contextBridge, ipcRenderer } = require("electron");
+const { contextBridge, ipcRenderer, webUtils } = require("electron");
 
 contextBridge.exposeInMainWorld("aiosDesktop", {
   chooseFolder: () => ipcRenderer.invoke("choose-folder"),
   chooseFile: options => ipcRenderer.invoke("choose-file", options),
-  openPath: target => ipcRenderer.invoke("open-path", target)
+  openPath: target => ipcRenderer.invoke("open-path", target),
+  getFilePath: file => webUtils.getPathForFile(file)
 });
